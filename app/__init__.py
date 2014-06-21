e5ab41ab3523c315db83c79a34af405a52b49629
Sidharta Noleto <sidvelloso@gmail.com>
2014-06-21 14:25:07 -0300

    Initial commit by yeoman py-gae

---
+++ app/__init__.py
@@ -0,0 +1,29 @@
+"""A simple webapp2 server."""
+
+import os
+
+import jinja2
+import webapp2
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+      template = JINJA_ENVIRONMENT.get_template('templates/index.html')
+      self.response.write(template.render({}))
+
+
+APPLICATION = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
+
+
+
+API = endpoints.api_server([])
+
